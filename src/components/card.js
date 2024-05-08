c875d1e9bc124fe10be1bdb9972ffdf5193279ff
Sofya Bogatyreva <bogatyreva.sofya@list.ru>
2024-05-08 13:57:03 +0300

    version_01

--- src/components/card.js
+++ src/components/card.js
@@ -1,76 +1,77 @@
-
-
 export function delElement(element) {
-    element.remove();
-  }
-
-  function handleResponse(response) {
-    if (response.ok) {
-        return response.json(); // Возвращает обещание, разрешенное с объектом данных JSON
-    } else {
-        throw new Error('Network response was not ok.'); // Выбрасывает ошибку, если ответ от сервера не успешен
-    }
-
-  }
- 
-  export function likeBtn(likeButton, _id) {
-    likeButton.classList.toggle("card__like-button_is-active");
-    const likesCount = document.querySelector('.card__likes');
-    const newLikesCount = Number(likesCount.textContent) + 1; // Увеличиваем количество лайков
-    fetch(`https://nomoreparties.co/v1/wff-cohort-12/cards/likes/${_id}`, {
-      method: "PUT",
-      headers: {
-          authorization: "e5e5de72-de46-4c51-be74-1878519f8c80",
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ likes: newLikesCount }) // Отправляем новое количество лайков на сервер
-  })
-    .then(handleResponse)
-    .then(data => {
-        console.log("Лайк успешно добавлен:", data);
-        likesCount.textContent = newLikesCount; // Обновляем интерфейс
-    })
-    .catch(error => {
-        console.error("Ошибка при добавлении лайка:", error);
-    });
+  element.remove();
 }
 
-  export function createCard(
-    nameValue,
-    linkValue,
-    likeHandler,
-    delHandler,
-    openHandler,
-    likes,
-    cardId
-  ) {
+const handleResponse = (response) => {
+  console.log(response);
+  return response.json();
+};
 
-    const template = document.querySelector("#card-template").content;
-    const cardElement = template.querySelector(".card").cloneNode(true);
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-    const likeButton = cardElement.querySelector(".card__like-button");
-    const likesCount = cardElement.querySelector('.card__likes');
-  
-    cardElement.querySelector(".card__image").src = linkValue;
-    cardElement.querySelector(".card__title").textContent = nameValue;
-    if (likes && likes !== null) {
-      likesCount.textContent = likes.length ? likes.length : 0;
-    } else {
-      likesCount.textContent = 0;
-    }
-  
-  
-    // Добавляем слушатель события на кнопку лайка
-    likeButton.addEventListener("click", () => {
-      likeHandler(likeButton, _id);
+export function likeBtn(likeButton, _id) {
+  // Определяем, есть ли уже лайк на этой карточке
 
+  const isActive = likeButton.classList.contains("card__like-button_is-active");
+  // Меняем состояние лайка
+  likeButton.classList.toggle("card__like-button_is-active");
+
+
+  // Отправляем запрос на сервер в зависимости от состояния лайка
+  fetch(`https://nomoreparties.co/v1/wff-cohort-12/cards/likes/${_id}`, {
+    method: isActive ? 'DELETE' : 'PUT',
+    headers: {
+      authorization: "e5e5de72-de46-4c51-be74-1878519f8c80",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      _id: _id,
+    }),
+  })
+    .then(handleResponse)
+    .then((data) => {
+      console.log(data);
+      const likesCount = likeButton.parentElement.querySelector(".card__likes");
+      // Обновляем счетчик лайков и состояние кнопки
+      likesCount.textContent = data.likes.length;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
-  
-    // Добавляем слушатель события на кнопку удаления
-    deleteButton.addEventListener("click", () => delHandler(cardElement));
-  
-    // Добавляем слушатель события на изображение
-    cardElement.querySelector(".card__image").addEventListener("click", openHandler);
-  
-    return cardElement;
+}
+export function createCard(
+  nameValue,
+  linkValue,
+  likeHandler,
+  delHandler,
+  openHandler,
+  likes,
+  _id
+) {
+  const template = document.querySelector("#card-template").content;
+  const cardElement = template.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const likesCount = cardElement.querySelector(".card__likes");
+
+  cardElement.querySelector(".card__image").src = linkValue;
+  cardElement.querySelector(".card__title").textContent = nameValue;
+  if (likes && likes !== null) {
+    likesCount.textContent = likes.length ? likes.length : 0;
+  } else {
+    likesCount.textContent = 0;
   }
+
+  // Добавляем слушатель события на кнопку лайка
+  likeButton.addEventListener("click", () => {
+    likeHandler(likeButton, _id);
+  });
+
+  // Добавляем слушатель события на кнопку удаления
+  deleteButton.addEventListener("click", () => delHandler(cardElement));
+
+  // Добавляем слушатель события на изображение
+  cardElement
+    .querySelector(".card__image")
+    .addEventListener("click", openHandler);
+
+  return cardElement;
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import { initialCards } from "./components/cards.js";
 
+// Импорт необходимых функций и стилей
+import { initialCards } from "./components/cards.js";
 import {
   clickOutsideHandler,
   closeModal,
   keyClickClose,
-  openModal
+  openModal,
 } from "./components/modal.js";
 import "./index.css";
-
-import {enableValidation} from "./components/validation.js";
-
+import { enableValidation } from "./components/validation.js";
 import { createCard, likeBtn, delElement } from "./components/card.js";
 import { data } from "autoprefixer";
 
+// Выбор DOM-элементов
 const addButton = document.querySelector(".profile__add-button");
 const editBtn = document.querySelector(".profile__edit-button");
 const popCaption = document.querySelector(".popup__caption");
@@ -22,15 +22,13 @@ const editPopUp = document.querySelector(".popup_type_edit");
 const popImg = document.querySelector(".popup_type_image");
 const addCard = document.querySelector(".popup_type_new-card");
 const formCard = document.querySelector(".popup_type_new-card .popup__form");
-
-
 const nameInputCard = document.querySelector(".popup__input_type_card-name");
 const descInputCard = document.querySelector(".popup__input_type_url");
 
+// Обработчик события для добавления новой карточки
 formCard.addEventListener("submit", (evt) => addCardNew(evt, placesList));
-// formCard.addEventListener("submit", addCardNew);
 
-//ОБРАБОТЧИК ОТКРЫТИЯ ПОПАПА РЕДАКТИРОВАНИЯ С ДАННЫМИ
+// Обработчик события для открытия попапа с данными для редактирования
 document.addEventListener("click", function (event) {
   if (event.target === editBtn) {
     const nameElement = document.querySelector(".profile__title");
@@ -44,103 +42,73 @@ document.addEventListener("click", function (event) {
   }
 });
 
-
-
+// Обработчики событий для открытия и закрытия попапов
 const popAddClose = document.querySelector(
   ".popup_type_new-card .popup__close"
 );
 const popEditClose = document.querySelector(".popup_type_edit .popup__close");
-
 addButton.addEventListener("click", () => openModal(addCard));
 editBtn.addEventListener("click", () => openModal(editPopUp));
-
-//ОБРАБОТЧИК ЗАКРЫТИЯ ПОПАПА ДОБАВЛЕНИЯ КАРТОЧКИ ЧЕРЕЗ КРЕСТИК
-popAddClose.addEventListener("click", () => {
-  closeModal(addCard);
-});
-
-//ОБРАБОТЧИК ЗАКРЫТИЯ ОКНА РЕДАКТИРОВАНИЯ ЧЕРЕЗ КРЕСТИК
-popEditClose.addEventListener("click", () => {
-  closeModal(editPopUp);
-});
-
-//СЛУШАТЕЛЬ ФУНКЦИИ ЗАКРЫТИЯ ПОПАПОВ ПРИ КЛИКЕ НА ФОН
+popAddClose.addEventListener("click", () => closeModal(addCard));
+popEditClose.addEventListener("click", () => closeModal(editPopUp));
 document.addEventListener("click", clickOutsideHandler);
 
-//ЗАКРЫТИЕ КАРТИНКИ ПО КРЕСТИКУ
+// Обработчик события для закрытия попапа с изображением
 const imgClose = document.querySelector(".popup_type_image .popup__close");
-
 imgClose.addEventListener("click", () => {
   closeModal(popImg);
 });
 
-//РЕДАКТИРОВАНИЕ ИМЕНИ И ИНФОРМАЦИИ О СЕБЕ
-const formProfleElement = document.querySelector(
-  ".popup_type_edit  .popup__form"
-);
-const nameInput = document.querySelector(".popup__input_type_name");
-const jobInput = document.querySelector(".popup__input_type_description");
-
-function handleFormProfileSubmit(evt) {
-  evt.preventDefault();
-
-  nameInput.value;
-  jobInput.value;
-  const profTitle = document.querySelector(".profile__title");
-  const profJob = document.querySelector(".profile__description");
-
-  profTitle.textContent = nameInput.value;
-  profJob.textContent = jobInput.value;
-
-  closeModal(editPopUp);
-}
-
-formProfleElement.addEventListener("submit", handleFormProfileSubmit);
-
+// Функция для добавления новой карточки
 const newCard = (name, link) => {
-  fetch('https://nomoreparties.co/v1/wff-cohort-12/cards', {
-    method:'POST',
+  return fetch("https://nomoreparties.co/v1/wff-cohort-12/cards", {
+    method: "POST",
     headers: {
-      authorization: 'e5e5de72-de46-4c51-be74-1878519f8c80',
-      'Content-Type': 'application/json'
+      authorization: "e5e5de72-de46-4c51-be74-1878519f8c80",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       name: name,
-      link: link
+      link: link,
+    }),
+  })
+    .then(handleResponse)
+    .then((data) => {
+      return data; // Возвращаем данные о добавленной карточке
     })
-  })
-  .then(handleResponse)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
-}
+    .catch((error) => {
+      console.error("Ошибка при получении данных:", error);
+    });
+};
 
-//Добавление новой карточки
+// Функция для обработки добавления новой карточки
 function addCardNew(evt, placesList) {
   evt.preventDefault();
   newCard(nameInputCard.value, descInputCard.value)
+    .then((data) => {
+      const newCardElement = createCard(
+        nameInputCard.value,
+        descInputCard.value,
+        likeBtn,
+        delElement,
+        openCard,
+        [], // Передаем пустой массив лайков для новой карточки
+        data._id // Передаем идентификатор новой карточки
+      );
 
-  const newCardElement = createCard(
-    nameInputCard.value,
-    descInputCard.value,
-    likeBtn,
-    delElement,
-    openCard,
-    cardId
-  );
+      placesList.insertBefore(newCardElement, placesList.firstChild);
 
-  placesList.insertBefore(newCardElement, placesList.firstChild);
+      nameInputCard.value = "";
+      descInputCard.value = "";
 
-  nameInputCard.value = "";
-  descInputCard.value = "";
-
-  closeModal(addCard);
+      closeModal(addCard);
+    })
+    .catch((error) => {
+      console.error("Ошибка при добавлении новой карточки:", error);
+    });
 }
 
-//ОТКРЫТИЕ КАРТОЧЕК
+// Функция для открытия карточки
 export function openCard(evt) {
   const cardImg = evt.target;
   const imgDesp =
@@ -151,122 +119,206 @@ export function openCard(evt) {
   openModal(popImg);
 }
 
-//ВЫЗЫВАЕМ ФУНКЦИЮ ДОБАВЛЕНИЯ ОШИБКИ К ФОРМАМ
+// Вызов функции для включения валидации форм
 enableValidation();
 
-
-
+// Функция для обработки ответа от API
 const handleResponse = (response) => {
-  console.log(response)
   return response.json();
 };
 
-const updateUserInfo = (userData) => {
-  const { name, about, avatar } = userData;
-  document.querySelector('.profile__title').textContent = name;
-  document.querySelector('.profile__description').textContent = about;
-  document.querySelector('.profile__image').style.backgroundImage = avatar;
+// Функция для обновления карточек
+const updateCards = (cardsData) => {
+  const cardsContainer = document.querySelector(".places__list");
+
+  cardsData.forEach((card) => {
+    const { name, link, likes, _id } = card;
+    const cardElement = createCard(
+      name,
+      link,
+      likeBtn,
+      delElement,
+      openCard,
+      likes,
+      _id
+    );
+    cardsContainer.appendChild(cardElement);
+  });
 };
 
-
-const getData = () => {
-  fetch('https://nomoreparties.co/v1/wff-cohort-12/users/me', {
-    method: 'GET',
+// Функция для получения карточек с сервера
+export const getCards = () => {
+  fetch("https://nomoreparties.co/v1/wff-cohort-12/cards ", {
+    method: "GET",
     headers: {
-      authorization: 'e5e5de72-de46-4c51-be74-1878519f8c80',
-      'Content-Type': 'application/json'
-    }
+      authorization: "e5e5de72-de46-4c51-be74-1878519f8c80",
+      "Content-Type": "application/json",
+    },
   })
-  .then(handleResponse)
+    .then(handleResponse)
+    .then((data) => {
+      updateCards(data);
+    })
+    .catch((error) => {
+      console.error("Ошибка при получении данных:", error);
+    });
+};
 
-  .then((data) => {
-    updateUserInfo(data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
+getCards();
+
+// Функция для обработки запросов к API
+const baseUrl = "https://nomoreparties.co/v1/wff-cohort-12";
+const headers = {
+  authorization: "276ad100-5acb-4462-bd4c-2c97ced4dc2a",
+  "Content-Type": "application/json",
+};
+
+const apiMethod = (apiRequest) => {
+  return fetch(`${baseUrl}/${apiRequest.url}`, {
+    method: `${apiRequest.method}`,
+    headers: headers,
+    body: JSON.stringify(apiRequest.body),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res.status);
   });
+};
+
+// Функция для обновления аватара
+const updateAvatarOnServer = (avatar) =>
+  apiMethod({
+    url: "users/me/avatar",
+    method: "PATCH",
+    body: { avatar: avatar },
+  });
+// Получаем элементы DOM
+const profImg = document.querySelector(".profile__image"); // Изображение профиля
+const popupUpdateAvatar = document.querySelector(".popup_type_update_avatar"); // Попап для обновления аватара
+const formUpdateAvatar = document.forms["update-avatar"]; // Форма для обновления аватара
+const inputUpdateAvatar = formUpdateAvatar.elements["link"]; // Поле ввода ссылки на аватар
+const avatarCloseModal = document.querySelector(
+  ".popup_type_update_avatar .popup__close"
+); // Кнопка закрытия попапа для обновления аватара
+
+// Обработчик события для открытия попапа при клике на изображение профиля
+profImg.addEventListener("click", () => {
+  profImg.classList.add("clicked");
+  inputUpdateAvatar.value = "";
+  openModal(popupUpdateAvatar);
+});
+
+// Обработчик события для удаления класса при уводе курсора с изображения профиля
+profImg.addEventListener("mouseout", () => profImg.classList.remove("clicked"));
+
+// Обработчик события для закрытия попапа при клике на кнопку закрытия
+avatarCloseModal.addEventListener("click", () => closeModal(popupUpdateAvatar));
+
+// Обработчик события для отправки формы обновления аватара
+formUpdateAvatar.addEventListener("submit", (evt) =>
+  submitUpdateAvatar(evt, inputUpdateAvatar.value)
+);
+
+// Функция для отправки запроса на обновление аватара
+function submitUpdateAvatar(evt, avatar) {
+  evt.preventDefault();
+  updateAvatarOnServer(avatar)
+    .then((data) => {
+      profImg.style.backgroundImage = `url(${data.avatar})`;
+      console.log("Ссылка на новый аватар:", data.avatar);
+      closeModal(popupUpdateAvatar);
+    })
+    .catch((error) => console.log("Ошибка при обновлении аватара:", error));
+   
+    
+
+  closeModal(popupUpdateAvatar);
+}
+
+
+// РЕДАКТИРОВАНИЕ ДАННЫХ ПОЛЬЗОВАТЕЛЯ
+
+
+// Функция для редактирования профиля
+const editProfile = (userData) => {
+  return fetch("https://nomoreparties.co/v1/wff-cohort-12/users/me", {
+    method: "PATCH",
+    headers: {
+      authorization: "e5e5de72-de46-4c51-be74-1878519f8c80",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(handleResponse)
+    .catch((error) => {
+      console.error("Ошибка при обновлении профиля:", error);
+      throw error;
+    });
+};
+
+// Получаем элементы формы редактирования профиля
+const formProfleElement = document.querySelector(
+  ".popup_type_edit  .popup__form"
+);
+const nameInput = document.querySelector(".popup__input_type_name"); // Поле ввода имени
+const jobInput = document.querySelector(".popup__input_type_description"); // Поле ввода информации о себе
+const avatarInput = document.querySelector(".popup__input_type_url"); // Поле ввода ссылки на аватар
+
+
+// Обновляем информацию о пользователе
+const updateUserInfo = (userData) => {
+  const { name, about, avatar } = userData;
+  document.querySelector(".profile__title").textContent = name;
+  document.querySelector(".profile__description").textContent = about;
+  document.querySelector(".profile__image").src= avatar;
+};
+
+// Обработчик события отправки формы редактирования профиля
+function handleFormProfileSubmit(evt) {
+  evt.preventDefault();
+
+  // Получаем значения из формы
+  const newName = nameInput.value;
+  const newAbout = jobInput.value;
+  const newAvatar = avatarInput.value;
+
+  // Отправляем изменения на сервер
+  editProfile({ name: newName, about: newAbout, avatar: newAvatar })
+    .then((data) => {
+      // Обновляем данные на клиенте
+      updateUserInfo(data);
+      console.log(data);
+      closeModal(editPopUp);
+    })
+    .catch((error) => {
+      console.error("Ошибка при обновлении профиля:", error);
+      // Обработка ошибки (например, вывод сообщения пользователю)
+    });
+}
+
+// Получаем данные о пользователе с сервера
+const getData = () => {
+  fetch("https://nomoreparties.co/v1/wff-cohort-12/users/me", {
+    method: "GET",
+    headers: {
+      authorization: "e5e5de72-de46-4c51-be74-1878519f8c80",
+      "Content-Type": "application/json",
+    },
+  })
+    .then(handleResponse)
+    .then((data) => {
+      updateUserInfo(data); // Обновляем информацию о пользователе
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Ошибка при получении данных:", error);
+    });
 };
 
 getData();
 
 
-const updateCards = (cardsData) => {
-  const cardsContainer = document.querySelector('.places__list');
-
-  cardsData.forEach(card => {
-    const { name, link, likes, _id } = card;
-    const cardElement = createCard(name, link, likeBtn, delElement, openCard, likes, _id);
-    cardsContainer.appendChild(cardElement);
-  });
-};
+formProfleElement.addEventListener("submit", handleFormProfileSubmit);
 
 
-
-
-export const getCards = () => {
-  fetch ('https://nomoreparties.co/v1/wff-cohort-12/cards ',{
-    method: 'GET',
-    headers: {
-      authorization: 'e5e5de72-de46-4c51-be74-1878519f8c80',
-      'Content-Type': 'application/json'
-    }
-  })
-
-  .then(handleResponse)
-  .then((data) => {
-    updateCards(data);
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
-}
-
-getCards();
-
-const editProfile = () =>{
-  fetch('https://nomoreparties.co/v1/wff-cohort-12/users/me',{
-  method: 'PATCH',
-  headers: {
-    authorization: 'e5e5de72-de46-4c51-be74-1878519f8c80',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    name: 'Mario',
-    about: 'Kekovich'
-  })
-})
-
-.then(handleResponse)
-  .then((data) => {
-    updateUserInfo(data);
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
-}
-
-editProfile();
-
-
-
-//РЕДАКТИРОВАНИЕ АВАТАРА
-const avatarEdit = () =>{
-  fetch('https://nomoreparties.co/v1/wff-cohort-12/users/me',{
-  method:'PATCH',
-  headers:{
-    authorization: 'e5e5de72-de46-4c51-be74-1878519f8c80',
-    'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      avatar:avatar,
-    })
-
-    .then(handleResponse)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  })
-})}
